8427134ee794526c83d9fe2d8a8845f1346ad721
cassiocappellari <cassiocappellari@gmail.com>
2021-04-05 08:17:21 -0300

    feat: create get user by id route

--- src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
@@ -9,7 +9,13 @@ class ShowUserProfileUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const checkIfUserExists = this.usersRepository.findById(user_id)
+
+    if (!checkIfUserExists) {
+      throw new Error("user not exists")
+    }
+
+    return checkIfUserExists;
   }
 }
 
