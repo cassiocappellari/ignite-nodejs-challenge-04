b644009a4efc3af66346b795887c4715b3fdfc4c
cassiocappellari <cassiocappellari@gmail.com>
2021-04-05 08:05:57 -0300

    feat: create list all users route

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,19 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const checkIfUserExists = this.usersRepository.findById(user_id)
+
+    if (!checkIfUserExists) {
+      throw new Error("user not exists")
+    }
+
+    if (checkIfUserExists.admin === false) {
+      throw new Error("unauthorized user")
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
